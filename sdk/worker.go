f9c98a7e1de27f459f9ad5f6a48bd9932f418eff
fffff <art67789@gmail.com>
2026-05-07 17:58:57 +0300

    feat: implement basic RegisterWorkflow function

--- sdk/worker.go
+++ sdk/worker.go
@@ -1,1 +1,38 @@
 package sdk
+
+import (
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+type Worker struct {
+	client *Client
+	queue  string
+
+	workflowFunctions map[string]reflect.Value
+}
+
+func NewWorker(c *Client, queue string) *Worker {
+	return &Worker{
+		client:            c,
+		queue:             queue,
+		workflowFunctions: make(map[string]reflect.Value),
+	}
+}
+
+func (w *Worker) RegisterWorkflow(fn interface{}) {
+	fnType := reflect.TypeOf(fn)
+
+	if fnType == nil || fnType.Kind() != reflect.Func {
+		panic("RegisterWorkflow: fn missing type, must be a function")
+	}
+
+	fnValue := reflect.ValueOf(fn)
+	fullName := runtime.FuncForPC(fnValue.Pointer()).Name()
+
+	words := strings.Split(fullName, ".")
+	name := words[len(words)-1]
+
+	w.workflowFunctions[name] = fnValue
+}
