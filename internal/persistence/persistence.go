65f654111bb517fe2809e97c3957d36e3ef02745
fffff <art67789@gmail.com>
2026-05-09 23:37:24 +0300

    feat: add persistence layer with workflow queries

---
+++ internal/persistence/persistence.go
@@ -0,0 +1,20 @@
+package persistence
+
+import (
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Persistence struct {
+	db *pgxpool.Pool
+}
+
+func New(db *pgxpool.Pool) *Persistence {
+	return &Persistence{
+		db: db,
+	}
+}
+
+func (p *Persistence) InsertWorkflowExecution(ctx, tx pgx.Tx, w WorkflowExecution) error
+func (p *Persistence) GetWorkflowExecution(ctx, workflowID, runID string) (*WorkflowExecution, error)
+func (p *Persistence) UpdateWorkflowStatus(ctx, tx pgx.Tx, workflowID, runID, status string) error
