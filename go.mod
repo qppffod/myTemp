1737bbf7ae530bf0e6e0100f53cdfcaf2c21a9b6
fffff <art67789@gmail.com>
2026-05-09 22:21:55 +0300

    feat(persistence): add func to run migrations
    Add runMigrations function to up the migration

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/qppffod/myTemp
 go 1.26.2
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
---
+++ internal/persistence/migrations.go
@@ -0,0 +1,20 @@
+package persistence
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func runMigrations(connStr string) error {
+	m, err := migrate.New("file://migrations", connStr)
+	if err != nil {
+		return fmt.Errorf("migrate.New: %w", err)
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("migrate.Up: %w", err)
+	}
+	return nil
+}
