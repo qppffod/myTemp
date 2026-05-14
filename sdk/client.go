a684c7a3480f25e2de6fe2866e5b110b01a45600
fffff <art67789@gmail.com>
2026-05-14 19:58:11 +0300

    feat: wrap engine RPC'c in Client methods

--- sdk/client.go
+++ sdk/client.go
@@ -32,6 +32,44 @@ func (c *Client) Close() {
 	c.conn.Close()
 }
 
-func (c *Client) StartNewWorkflow(ctx context.Context, workflowType, taskQueue string, data any) {
-	c.engine.StartWorkflow(ctx, &pb.StartWorkflowRequest{})
+func (c *Client) StartNewWorkflow(ctx context.Context, workflowID, workflowType, taskQueue string, input []byte) (string, error) {
+	resp, err := c.engine.StartWorkflow(ctx, &pb.StartWorkflowRequest{
+		WorkflowId:   workflowID,
+		WorkflowType: workflowType,
+		TaskQueue:    taskQueue,
+		Input:        input,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.RunId, nil
+}
+
+func (c *Client) PollWorkflowTask(ctx context.Context, queue string) (*pb.PollWorkflowTaskResponse, error) {
+	return c.engine.PollWorkflowTask(ctx, &pb.PollWorkflowTaskRequest{TaskQueue: queue})
+}
+
+func (c *Client) RespondWorkflowTaskCompleted(ctx context.Context, taskID int64, workflowID, runID string, commands []*pb.Command) error {
+	_, err := c.engine.RespondWorkflowTaskCompleted(ctx, &pb.RespondWorkflowTaskCompletedRequest{
+		TaskId:     taskID,
+		WorkflowId: workflowID,
+		RunId:      runID,
+		Commands:   commands,
+	})
+	return err
+}
+
+func (c *Client) PollActivityTask(ctx context.Context, queue string) (*pb.PollActivityTaskResponse, error) {
+	return c.engine.PollActivityTask(ctx, &pb.PollActivityTaskRequest{TaskQueue: queue})
+}
+
+func (c *Client) RespondActivityTaskCompleted(ctx context.Context, taskID int64, workflowID, runID string, result []byte) error {
+	_, err := c.engine.RespondActivityTaskCompleted(ctx, &pb.RespondActivityTaskCompletedRequest{
+		TaskId:     taskID,
+		WorkflowId: workflowID,
+		RunId:      runID,
+		Result:     result,
+	})
+	return err
 }
