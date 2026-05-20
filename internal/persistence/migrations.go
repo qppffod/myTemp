1ac714deb93869ec4996539c0d5b9ee8585154d1
fffff <art67789@gmail.com>
2026-05-20 23:25:03 +0300

    fix: resolve "unknown driver 'file'" on engine startup

--- internal/persistence/migrations.go
+++ internal/persistence/migrations.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func RunMigrations(connStr string) error {
