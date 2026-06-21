e692ea15562f7d93daf7b868db6509f7c3ea6f71
fffff <art67789@gmail.com>
2026-06-21 23:54:10 +0300

    test: add test utilities

---
+++ internal/testutil/postgres.go
@@ -0,0 +1,61 @@
+package testutil
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/qppffod/myTemp/internal/history"
+	"github.com/qppffod/myTemp/internal/persistence"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func SetupEngine(t *testing.T) (*history.History, *persistence.Persistence, *pgxpool.Pool) {
+	t.Helper()
+	pool, clean := StartTestPostgres(t)
+	defer clean()
+	p := persistence.New(pool)
+	h := history.New(p)
+	return h, p, pool
+}
+
+func StartTestPostgres(t *testing.T) (*pgxpool.Pool, func()) {
+	t.Helper()
+	ctx := context.Background()
+
+	container, err := postgres.Run(ctx, "postgres:16",
+		postgres.WithDatabase("test"),
+		postgres.WithUsername("test"),
+		postgres.WithPassword("test"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2)),
+	)
+	require.NoError(t, err)
+
+	connStr, err := container.ConnectionString(ctx, "sslmode=disable")
+	require.NoError(t, err)
+
+	require.NoError(t, persistence.RunMigrations(connStr))
+
+	pool, err := pgxpool.New(ctx, connStr)
+	require.NoError(t, err)
+
+	cleanunp := func() {
+		pool.Close()
+		container.Terminate(ctx)
+	}
+
+	return pool, cleanunp
+}
+
+func GetEvents(t *testing.T, db *pgxpool.Pool, workflowID, runID string) []persistence.Event {
+	t.Helper()
+	p := persistence.New(db)
+	events, err := p.GetEvents(t.Context(), workflowID, runID)
+	require.NoError(t, err)
+	return events
+}
