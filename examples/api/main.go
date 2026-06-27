6ff567177313ace5ca32a055e32a691b0e3e17d3
fffff <art67789@gmail.com>
2026-06-28 01:22:14 +0300

    feat: add observability

--- examples/api/main.go
+++ examples/api/main.go
@@ -20,6 +20,7 @@ func main() {
 	// mux.HandleFunc("POST /test", TestHandler(client))
 	mux.HandleFunc("POST /test", TestHandler(client))
 	mux.HandleFunc("POST /approve", ApproveHandler(client))
+	mux.HandleFunc("POST /order", OrderHandler(client))
 
 	server := http.Server{
 		Addr:    ":3000",
@@ -57,6 +58,31 @@ func TestHandler(c *sdk.Client) http.HandlerFunc {
 		}
 
 		w.Write([]byte(fmt.Sprintf("runID:%s", runID)))
+	}
+}
+
+func OrderHandler(c *sdk.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var order PizzaOrder
+		if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
+			http.Error(w, fmt.Sprintf("json decoder error: %s", err.Error()), 400)
+			return
+		}
+
+		orderBytes, err := json.Marshal(order)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("json marshal error: %s", err.Error()), 500)
+			return
+		}
+
+		workflowID := fmt.Sprintf("order-%d", order.OrderID)
+		runID, err := c.StartNewWorkflow(r.Context(), workflowID, "TestWorkflow", "test", orderBytes)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("StartNewWorkflow failed: %s", err.Error()), 500)
+			return
+		}
+
+		w.Write([]byte(fmt.Sprintf("workflowID:%s runID:%s", workflowID, runID)))
 	}
 }
 
